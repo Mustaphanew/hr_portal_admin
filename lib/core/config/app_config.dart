b5bef7d28c5b47f19f9734f4c08fcfe3fc9ee1ef
hiethm9980-coder <hiethm9980@gmail.com>
2026-04-30 15:37:16 +0300

    Update app_config.dart

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -65,6 +65,10 @@ class AppConfig {
         const devFallback = 'http://10.0.2.2:8000';
         return AppConfig(
           flavor: AppFlavor.dev,
+<<<<<<< Updated upstream
+=======
+          // baseUrl: 'http://192.168.137.230:8000',
+>>>>>>> Stashed changes
           baseUrl: devBaseUrl,
           envName: 'Development',
           enableDebugLogs: true,
